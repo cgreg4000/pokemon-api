5dd264b966a80354cbee4250826fc7465f7f4df4
cgreg4000 <calebggregory@gmail.com>
2022-03-14 13:52:16 -0500

    react app that fetches names of all pokemon

---
+++ src/components/PokemonApi.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+const PokemonApi = () => {
+
+    let [pokemonList, setPokemonList] = useState([])
+
+    const getPokemon = () => {
+        console.log("starting")
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=1126")
+            .then((response) => {
+                return response.json()
+            })
+            .then((response) => {
+                setPokemonList(response.results)
+            })
+    }
+
+    return (
+        <>
+            <button className="btn btn-secondary mt-5" onClick={getPokemon}>Fetch Pokemon</button>
+            <ul className="mt-4">
+                {
+                    pokemonList.map((pokemon, index) => {
+                        return (
+                            <li key={index}>{pokemon.name}</li>
+                        )
+                    })
+                }
+            </ul>
+        </>
+    )
+}
+
+export default PokemonApi
